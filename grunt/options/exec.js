738f51d32ca5aba6b38be2712e472b7176d9ca78
Gizburdt <gizburdt@gmail.com>
2014-02-18 10:38:11 +0100

    Remove unused files

--- grunt/options/exec.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(grunt) {
-    return {
-    	wp_download: {
-    		command: 'wp core download --locale=<%= wp.download.locale %>  --version=<%= wp.download.version %>'
-    	}
-    }
-}
